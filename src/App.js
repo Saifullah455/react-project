cd16cfaebb7181fce807da7276eb59b187aaee8b
Saifullah455 <41240651+Saifullah455@users.noreply.github.com>
2020-06-09 21:10:15 -0600

    Updated App

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Updated by SAIFULLAH-BIHARI
         </p>
         <a
           className="App-link"
